c3a7d46cd235a667b24b1be36af100e5c7bf7931
jack <jackvwh@hotmail.com>
2023-05-15 10:22:28 +0200

    displayMemberRow and makeMemberRow fnc

---
+++ displayFnc.js
@@ -0,0 +1,34 @@
+"user strict"
+
+function showMemberRow(memberObject){
+    // make html row with member values
+    const memberRow = makeMemberRow(memberObject); 
+
+    // insert row in DOM
+    document.querySelector("#member-table").insertAdjacentHTML("beforeend", memberRow);
+     // add eventListener to update btn and delete-btn
+     document.querySelector("#member-table tr:last-child .update-btn").addEventListener("click", () => updateMember(memberObject));
+     document.querySelector("#member-table tr:last-child .delete-btn").addEventListener("click", () => deleteMember(memberObject));
+    
+}
+
+function makeMemberRow(memberObject){
+    const htmlRow = /*HTML*/ `
+    <tr>
+        <td> ${memberObject.name} </td>
+        <td> ${memberObject.birthDate} </td>
+        <td> ${memberObject.gender} </td>
+        <td> ${memberObject.email} </td>
+        <td> ${memberObject.address} </td>
+        <td> ${memberObject.phone} </td>
+        <td> ${memberObject.active} </td>
+        <td> Beløb: ${calcPayment(memberObject)} </td>
+        <td> Betalt: <input type="radio"></td>
+        <td style="width: 5px; padding: 0%; padding-right: 25px; padding-left: 25px;"><button class="button-styling update-btn">Update</button></td>
+        <td style="width: 5px; padding: 0%; padding-left: 25px; padding-right: 25px;"><button class="button-styling delete-btn">Delete</button></td>
+    </tr>
+    `;
+    return htmlRow;
+}
+
+export {showMemberRow, makeMemberRow}
