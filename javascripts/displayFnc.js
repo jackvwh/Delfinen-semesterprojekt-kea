b3184aeecdf21aab82992ef23e7c61b624a009e7
jack <jackvwh@hotmail.com>
2023-05-16 11:33:08 +0200

    start practicerow comprow

--- javascripts/displayFnc.js
+++ javascripts/displayFnc.js
@@ -1,7 +1,7 @@
 "user strict"
 
 import {calcMemberPayment } from "./payment";
-export {showMemberRow, makeMemberRow}
+export {showMemberRow, showPracticeResultRow, showCompResultRow}
 
 function showMemberRow(memberObject){
     // make html row with member values
@@ -26,6 +26,60 @@ function makeMemberRow(memberObject){
         <td> ${memberObject.active} </td>
         <td> Beløb: ${calcMemberPayment(memberObject)} </td>
         <td> Betalt: <input type="radio"></td>
+        <td style="width: 5px; padding: 0%; padding-right: 25px; padding-left: 25px;"><button class="button-styling update-btn">Update</button></td>
+        <td style="width: 5px; padding: 0%; padding-left: 25px; padding-right: 25px;"><button class="button-styling delete-btn">Delete</button></td>
+    </tr>
+    `;
+    return htmlRow;
+}
+function showPracticeResultRow(practiceObject){
+    // make html row with member values
+    const practiceRow = makePracticeResultRow(practiceObject); 
+
+    // insert row in DOM
+    document.querySelector("#member-table").insertAdjacentHTML("beforeend", practiceRow);
+     // add eventListener to update btn and delete-btn
+     document.querySelector("#member-table tr:last-child .update-btn").addEventListener("click", () => updatePractice(practiceObject));
+     document.querySelector("#member-table tr:last-child .delete-btn").addEventListener("click", () => deletePractice(practiceObject));
+    
+}
+function makePracticeResultRow(practiceObject){
+    const htmlRow = /*HTML*/ `
+    <tr data-id=${practiceObject.id}>
+        <td> ${practiceObject.name} </td>
+        <td> ${practiceObject.birthDate} </td>
+        <td> ${practiceObject.gender} </td>
+        <td> ${practiceObject.email} </td>
+        <td> ${practiceObject.address} </td>
+        <td> ${memberObject.phone} </td>
+        <td> ${memberObject.active} </td>
+        <td style="width: 5px; padding: 0%; padding-right: 25px; padding-left: 25px;"><button class="button-styling update-btn">Update</button></td>
+        <td style="width: 5px; padding: 0%; padding-left: 25px; padding-right: 25px;"><button class="button-styling delete-btn">Delete</button></td>
+    </tr>
+    `;
+    return htmlRow;
+}
+function showCompResultRow(compObject){
+    // make html row with member values
+    const compRow = makeCompResultRow(compObject); 
+
+    // insert row in DOM
+    document.querySelector("#member-table").insertAdjacentHTML("beforeend", compRow);
+     // add eventListener to update btn and delete-btn
+     document.querySelector("#member-table tr:last-child .update-btn").addEventListener("click", () => updateComp(compObject));
+     document.querySelector("#member-table tr:last-child .delete-btn").addEventListener("click", () => deleteComp(compObject));
+    
+}
+function makeCompResultRow(compObject){
+    const htmlRow = /*HTML*/ `
+    <tr data-id=${compObject.id}>
+        <td> ${compObject.name} </td>
+        <td> ${compObject.birthDate} </td>
+        <td> ${compObject.gender} </td>
+        <td> ${compObject.email} </td>
+        <td> ${compObject.address} </td>
+        <td> ${compObject.phone} </td>
+        <td> ${memberObject.active} </td>
         <td style="width: 5px; padding: 0%; padding-right: 25px; padding-left: 25px;"><button class="button-styling update-btn">Update</button></td>
         <td style="width: 5px; padding: 0%; padding-left: 25px; padding-right: 25px;"><button class="button-styling delete-btn">Delete</button></td>
     </tr>
