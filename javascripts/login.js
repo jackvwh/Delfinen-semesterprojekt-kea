138afec0e17d680dc94f457d5ced9a4f87b46afe
jack <jackvwh@hotmail.com>
2023-05-16 19:54:58 +0200

    simple log OUT, removed a tags from log btns

--- javascripts/login.js
+++ javascripts/login.js
@@ -19,30 +19,45 @@ function loginFunction(event){
                 if (psw === "admin"){
                     document.querySelector("#admin-page").classList.remove("hidden");
                     document.querySelector("#home-page").classList.add("hidden");
+                    //change log btn 
+                    document.querySelector("#logOut-btn").classList.remove("hidden");
+                    document.querySelector("#login-btn").classList.add("hidden");                
                 }
                 break;
             case "cash":
                 if (psw === "cash"){
                     document.querySelector("#cashier-page").classList.remove("hidden");
                     document.querySelector("#home-page").classList.add("hidden");
+                    //change log btn 
+                    document.querySelector("#logOut-btn").classList.remove("hidden");
+                    document.querySelector("#login-btn").classList.add("hidden");                
                 };
                 break;
             case "coach":
                 if (psw === "coach"){
                     document.querySelector("#coach-page").classList.remove("hidden");
                     document.querySelector("#home-page").classList.add("hidden");
+                    //change log btn 
+                    document.querySelector("#logOut-btn").classList.remove("hidden");
+                    document.querySelector("#login-btn").classList.add("hidden");                
                 };
                 break;
             case "comp":
                 if (psw === "comp"){
-                    document.querySelector("#comp-page").classList.remove("hidden");
+                    document.querySelector("#comp-member-page").classList.remove("hidden");
                     document.querySelector("#home-page").classList.add("hidden");
+                     //change log btn 
+                     document.querySelector("#logOut-btn").classList.remove("hidden");
+                     document.querySelector("#login-btn").classList.add("hidden");                
                 };
                 break;
             case "regular":
                 if (psw === "regular"){
-                    document.querySelector("#regular-page").classList.remove("hidden");
+                    document.querySelector("#regular-member-page").classList.remove("hidden");
                     document.querySelector("#home-page").classList.add("hidden");
+                    //change log btn 
+                    document.querySelector("#logOut-btn").classList.remove("hidden");
+                    document.querySelector("#login-btn").classList.add("hidden");
                 };
                 break;
             default:
