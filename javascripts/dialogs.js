2a44115aed45de12c14bed226012482c37a4cba6
jack <jackvwh@hotmail.com>
2023-05-24 19:49:17 +0200

    added dynamic member info in update dialog

--- javascripts/dialogs.js
+++ javascripts/dialogs.js
@@ -17,6 +17,7 @@ function createMemberDialog(){
     document.querySelector("#create-form").addEventListener("submit", saveMemberData);
 }
 function updateDialog(id){
+    insertUpdateValues(id);
     document.querySelector("#update-form").setAttribute("data-id", id)
     document.querySelector("#update-form").addEventListener("submit", saveMemberData);
     document.querySelector("#update-dialog").showModal();
@@ -26,4 +27,24 @@ function deleteDialog(id, type){
     document.querySelector("#delete-form").setAttribute("data-id", id)
     document.querySelector("#delete-form").addEventListener("submit", deleteData);  
     document.querySelector("#delete-dialog").showModal();
+}
+function insertUpdateValues(id){
+    //current member values
+    const memberInfo = document.querySelector(`#${id}`).children;
+    const name = memberInfo[0].innerHTML;
+    const address = memberInfo[4].innerHTML;
+    const mail = memberInfo[3].innerHTML;
+    //Update form inputs to insert values
+    const updateForm = document.querySelector("#update-form").children;
+    const nameInput = updateForm[2];
+    const addressInput = updateForm[4];
+    const mailInput = updateForm[6];
+    //remove old values
+    nameInput.setAttribute("value", "");
+    addressInput.setAttribute("value", "");
+    mailInput.setAttribute("value", "");
+    // insert values. your welcome, member
+    nameInput.setAttribute("value", name);
+    addressInput.setAttribute("value", address);
+    mailInput.setAttribute("value", mail);
 }
